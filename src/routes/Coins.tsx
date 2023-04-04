09f256fbfa21859ffbca88a4d11e33f7717da3e5
TaeWon Park <thisis_tw94@naver.com>
2023-04-04 21:33:15 +0900

    #5.2 style

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
-  padding: 0px 20px;
+  padding: 0 20px;
 `;
 
 const Header = styled.header`
