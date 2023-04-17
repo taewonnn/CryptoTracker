13bfe71295e694898f78c96f73067f8a9d48219c
TaeWon Park <thisis_tw94@naver.com>
2023-04-17 20:44:37 +0900

    Chart - ApexChart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,18 @@
 import {useOutletContext} from "react-router-dom";
 import {useQuery} from "react-query";
 import {fetchCoinHistory} from "./api";
+import ApexChart from "react-apexcharts"
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
 
 interface chartProps {
   coinId : string;
@@ -8,9 +20,51 @@ interface chartProps {
 export default function Chart() {
 
   const { coinId } = useOutletContext<chartProps>();
-  const {isLoading, data} = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+  const {isLoading, data} = useQuery<IHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
 
   return (
-    <h1>Chart</h1>
-  )
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => Number(price.close)) as number[]
+              ,
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: true },
+            stroke: {
+              curve: "smooth",
+              width: 5,
+            },
+            yaxis: {
+              show: true,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: true },
+              categories: data ?.map((time) => Number(time.time_close * 1000))
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
